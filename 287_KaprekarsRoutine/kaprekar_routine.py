4da48d591cfb004395bda18f60a35519da14ea89
Mateus Lopes Teixeira <lopesteixeiramateus@gmail.com>
2017-03-02 00:39:31 -0300

    List comprehension implemented

--- 287_KaprekarsRoutine/kaprekar_routine.py
+++ 287_KaprekarsRoutine/kaprekar_routine.py
@@ -5,24 +5,52 @@ Given a 4-digit number, return its largest digit
 
 Bonus 1: Given a 4-digit number, return them in descending order
 Bonus 2: Write a function that counts the number of iterations in Kaprekar's Routine, which is as follows.
-Given a 4-digit number that has at least two different digits, take that number's descending digits, and subtract that number's ascending digits. For example, given 6589, you should take 9865 - 5689, which is 4176. Repeat this process with 4176 and you'll get 7641 - 1467, which is 6174.
+\nGiven a 4-digit number that has at least two different digits, take that number's descending digits, and subtract that number's ascending digits. For example, given 6589, you should take 9865 - 5689, which is 4176. Repeat this process with 4176 and you'll get 7641 - 1467, which is 6174.
 
 '''
 
+import os.path
+import random
+
+'''
+Creates an input file (if it doesn't already exists)
+with a bunch of random numbers to run as input
+'''
+def create_input_file():
+	if(os.path.exists("./input.txt")):
+		return
+	else:
+		with open("input.txt", "w") as f:
+			for i in range(0, 10000):
+				num = random.randrange(100, 9999)
+				f.write(str(num) + "\n")
+			f.close()
+		return
+
 def largest_digit(number):
-	separate_numbers = []
-	
 	if(number > 9999):
 		print("The input has to be 4-digit or less")
 		return
 
-	for i in str(number):
-		separate_numbers.append(int(i))
-	
-	print("largest_digit({0}) -> {1}".format(number, max(separate_numbers)))
-	return int(max(separate_numbers))
+	#Using list comprehension:
+	separate_numbers =[int(i) for i in str(number)]
+
+	# Using a for loop
+	# separate_numbers = []
+	# for i in str(number):
+	# 	separate_numbers.append(int(i))	
+
+	largest = int(max(separate_numbers))
+	print("largest_digit({0}) -> {1}".format(number, largest))
+
+	return largest
 
 print("/r/DailyProgrammer Challenge #287 - Kaprekar's Routine")
+
+# create_input_file()
+# with open("input.txt", "r") as f:
+# 	for line in f:
+# 		largest_digit(int(line))
 
 largest_digit(3456)
 largest_digit(3222)
