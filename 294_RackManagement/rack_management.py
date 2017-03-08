da5e62796760eea5e8999da64c53d4864bb1318e
Mateus Lopes Teixeira <lopesteixeiramateus@gmail.com>
2017-03-08 16:53:51 -0300

    Bonus 1 added

--- 294_RackManagement/rack_management.py
+++ 294_RackManagement/rack_management.py
@@ -13,14 +13,24 @@ word list that can be formed using the tiles.
 '''
 
 def scrabble(rack, word):
+	print("scrabble({0}, {1})".format(rack, word))
 	rack = list(rack)
 
 	for letter in word:
 		if letter in rack:
 			rack.remove(letter)
-			print(letter)
 		else:
-			return False
+			if "?" in rack:
+				rack.remove("?")
+			else:
+				print(" -> false")
+				return False
+	print(" -> true")
 	return True
 
 scrabble("mateos", "mateus")	
+scrabble("seutam", "mateus")
+scrabble("udud", "dudu")
+scrabble("mariaeduarda", "dudu")
+scrabble("d?du", "dudu")
+scrabble("????", "dudu")
